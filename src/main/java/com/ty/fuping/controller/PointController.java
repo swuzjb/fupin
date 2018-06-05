c8bfaeda03628966bd325e99b74a4221e9a726ce
ty <731432318@qq.com>
2018-06-05 21:32:28 +0800

    评分更改问题解决

--- src/main/java/com/ty/fuping/controller/PointController.java
+++ src/main/java/com/ty/fuping/controller/PointController.java
@@ -1,6 +1,7 @@
 package com.ty.fuping.controller;
 
 import com.ty.fuping.entity.*;
+import com.ty.fuping.repository.PointRepository;
 import com.ty.fuping.service.AssessmentIndexService;
 import com.ty.fuping.service.PointService;
 import org.json.simple.JSONArray;
@@ -23,12 +24,23 @@ public class PointController {
     private PointService pointService;
     @Autowired
     private AssessmentIndexService assessmentIndexService;
+    @Autowired
+    private PointRepository pointRepository;
 
     @RequestMapping("/point")
     //评分
-    public Point  point(@RequestParam("objectId") Integer objectId,@RequestParam("assessmentIndexId") Integer assessmentIndexId,@RequestParam("point") Double duPoint){
-        Point point=new Point();
-        AssessmentObject assessmentObject=new AssessmentObject();
+    public Point point(@RequestParam("objectId") Integer objectId, @RequestParam("assessmentIndexId") Integer assessmentIndexId, @RequestParam("point") Double duPoint) {
+        Point point;
+        //查询是否已经有这条打分记录了
+        point = pointService.getPointByIndexAndObject(assessmentIndexId, objectId);
+        //如果有了直接更改保存
+        if (point != null) {
+            Double oldPoint = point.getPoint();
+            point.setPoint(duPoint);
+            return pointService.point(point, oldPoint);
+        }
+        point = new Point();
+        AssessmentObject assessmentObject = new AssessmentObject();
         AssessmentIndex assessmentIndex = assessmentIndexService.getAssessmentIndexbyId(assessmentIndexId);
         if (assessmentIndex == null)
             return null;
@@ -36,27 +48,27 @@ public class PointController {
         point.setAssessmentIndex(assessmentIndex);
         point.setAssessmentObject(assessmentObject);
         point.setPoint(duPoint);
-        return pointService.point(point);
+        return pointService.point(point, 0d);
     }
 
     @RequestMapping("/getassessmentindexscorebyplan")
     //通过考核计划Id获取打分详细
-    public JSONArray getPointByTownId(@RequestParam("assessmentPlanId") Integer assessmentPlanId,@RequestParam("assessmentObjectId") Integer assessmentObjectId){
-        List<AssessmentIndex> assessmentIndexList=assessmentIndexService.getAssessmentIndexbyPlanId(assessmentPlanId);
-        JSONArray jsonArray=new JSONArray();
+    public JSONArray getPointByTownId(@RequestParam("assessmentPlanId") Integer assessmentPlanId, @RequestParam("assessmentObjectId") Integer assessmentObjectId) {
+        List<AssessmentIndex> assessmentIndexList = assessmentIndexService.getAssessmentIndexbyPlanId(assessmentPlanId);
+        JSONArray jsonArray = new JSONArray();
         JSONObject jsonObject;
         AssessmentIndex assessmentIndex;
         Point point;
-        Iterator it=assessmentIndexList.iterator();
-        while (it.hasNext()){
-            assessmentIndex=(AssessmentIndex)it.next();
-            point=pointService.getPointByIndexAndObject(assessmentIndex.getAssessmentIndexId(),assessmentObjectId);
-            if(point==null){
-                point=new Point();
+        Iterator it = assessmentIndexList.iterator();
+        while (it.hasNext()) {
+            assessmentIndex = (AssessmentIndex) it.next();
+            point = pointService.getPointByIndexAndObject(assessmentIndex.getAssessmentIndexId(), assessmentObjectId);
+            if (point == null) {
+                point = new Point();
                 point.setPoint(new Double(0));
             }
-            jsonObject=new JSONObject();
-            jsonObject.put("point",point.getPoint());
+            jsonObject = new JSONObject();
+            jsonObject.put("point", point.getPoint());
             jsonObject.put("assessmentIndexId", assessmentIndex.getAssessmentIndexId());
             jsonObject.put("assessmentPlanId", assessmentIndex.getAssessmentPlan().getAssessmentPlanId());
             jsonObject.put("assessmentPlanTitle", assessmentIndex.getAssessmentPlan().getTitle());
--- src/main/java/com/ty/fuping/service/PointService.java
+++ src/main/java/com/ty/fuping/service/PointService.java
@@ -23,43 +23,48 @@ public class PointService {
     @Autowired
     private AssessmentPointRepository assessmentPointRepository;
 
-    @Transactional
     //给考核指标打分
-    public Point point(Point point) {
+    @Transactional
+    public Point point(Point point, Double oldPoint) {
         Point p = pointRepository.save(point);
+        //查出此人当前总得分
         AssessmentPoint assessmentPoint = assessmentPointRepository.findByAssessmentObjectOrderByPoint(point.getAssessmentObject());
         if (assessmentPoint == null) {
             assessmentPoint = new AssessmentPoint();
             assessmentPoint.setAssessmentObject(point.getAssessmentObject());
         }
+        //assessmentPoint中的原来得分
         Double inpoint = assessmentPoint.getPoint();
-        inpoint = point.getPoint();
-        assessmentPoint.setPoint(inpoint);
+        //设置最新得分：原来的得分加上新打的分减去此考核指标原来的分
+        assessmentPoint.setPoint(inpoint + point.getPoint() - oldPoint);
         assessmentPointRepository.save(assessmentPoint);
         return p;
     }
 
-    @Transactional
+
     //删除记录by考核指标
+    @Transactional
     public void deletePointByAssessmentIndex(AssessmentIndex assessmentIndex) {
         pointRepository.deleteAllByAssessmentIndex_AssessmentIndexId(assessmentIndex.getAssessmentIndexId());
     }
 
-    @Transactional
+
     //删除记录by考核对象
+    @Transactional
     public void deletePointByAssessmentObject(AssessmentObject assessmentObject) {
         pointRepository.deleteAllByAssessmentObject(assessmentObject);
     }
 
-    @Transactional
+
     //通过考核指标和考核对象Id获取打分
-    public Point getPointByIndexAndObject(Integer indexId, Integer objectId){
-        return pointRepository.findByAssessmentIndex_AssessmentIndexIdAndAssessmentObject_AssessmentObjectId(indexId,objectId);
+    @Transactional
+    public Point getPointByIndexAndObject(Integer indexId, Integer objectId) {
+        return pointRepository.findByAssessmentIndex_AssessmentIndexIdAndAssessmentObject_AssessmentObjectId(indexId, objectId);
     }
 
     @Transactional
     //通过考核指标获取打分
-    public List<Point> getPointByIndexId(Integer indexId){
+    public List<Point> getPointByIndexId(Integer indexId) {
         return pointRepository.findAllByAssessmentIndex_AssessmentIndexId(indexId);
     }
 
